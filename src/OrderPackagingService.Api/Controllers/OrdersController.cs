c84e8bdc3f65cd433b2ada5f71225f65edee4dd0
MarcosAllysson <marcosgracip05@gmail.com>
2025-05-28 20:35:45 -0300

    feat: implemented order controller

---
+++ src/OrderPackagingService.Api/Controllers/OrdersController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OrderPackagingService.Domain.Services;
+using OrderPackagingService.Shared.Dtos;
+
+namespace OrderPackagingService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class OrdersController : Controller
+    {
+        private readonly IPackingService _packingService;
+
+        public OrdersController(IPackingService packingService)
+        {
+            _packingService = packingService;
+        }
+
+        [Authorize]
+        [HttpPost("pack")]
+        public IActionResult PackOrders(List<OrderRequestDto> orders)
+        {
+            try
+            {
+                return Ok(_packingService.PackOrders(orders));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
